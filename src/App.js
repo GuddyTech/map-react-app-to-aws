3bbc18670317a84e424baf55d9f9068b312532d8
guddytech <guddytechs@gmail.com>
2023-08-09 23:29:36 +0100

    changed the code to a time displaying application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    // Update the current time every second
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Clean up the interval on component unmount
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []); // Empty array means this effect runs only once after initial render
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Guddy's Map App
-        </a>
-      </header>
+      <h1>Current Time</h1>
+      <p>{currentTime.toLocaleTimeString()}</p>
     </div>
   );
 }
